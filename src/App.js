653079410927e4615f0c306263c742b9aab23fba
iamnizamul <itsmenizamul@gmail.com>
2023-11-05 15:54:42 +0530

    Displaying all the added todos

--- src/App.js
+++ src/App.js
@@ -15,16 +15,25 @@ function App() {
       </div>
       <div className="input">
         <input value={toDo} onChange={(e) => setToDo(e.target.value)} type="text" placeholder="🖊️ Add item..." />
-        <i onClick={() => setToDos([...toDos, {text: toDo, status: false}])} className="fas fa-plus"></i>
+        <i onClick={() => setToDos([...toDos, {id: Date.now(), text: toDo, status: false}])} className="fas fa-plus"></i>
       </div>
       <div className="todos">
         {
-          toDos.map((value) => {
+          toDos.map((obj) => {
             return (
-              <div className="todo">
+              <div className="todo" key={obj.id}>
           <div className="left">
-            <input type="checkbox" name="" id="" />
-            <p>{value.text}</p>
+            <input onChange={(e) =>{
+              console.log(e.target.checked)
+              console.log(obj)
+              setToDos(toDos.filter((obj2)=> {
+                if(obj2.id === obj.id) {
+                  obj2.status = e.target.checked
+                }
+                return obj2
+              }))
+            }} type="checkbox" value={obj.status} name="" id="" />
+            <p>{obj.text}</p>
           </div>
           <div className="right">
             <i className="fas fa-times"></i>
@@ -34,7 +43,17 @@ function App() {
           })
           
         }
+
+        {
+          toDos.map((obj) => {
+            if(obj.status) {
+              return ( <h1 key={obj.id}>{obj.text}</h1> )
+            }
+            return null
+          })
+        }
         </div>
+
         
     </div>
   );
